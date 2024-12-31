779b4fa13d3569226245c4769f96b1823d8f1c72
htt-py <113143108+htt-py@users.noreply.github.com>
2024-12-31 15:15:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
+const fastify = require("fastify")({
+  bodyLimit: 1024 * 1024 * 1024,
+  logger: true,
+});
+const axios = require("axios");
+const { MEDAL_URL, ORACLE_URL, KONSTANT_URL } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+fastify.post("/decompile", async (request, reply) => {
+  let { key } = request.headers;
+  if (!key) ({ key } = request.query);
+  const body = request.body;
+  const headers = { "Content-Type": "application/json" };
+  let url = ORACLE_URL; // Default URL
+  let payload;
+  if (key?.startsWith("Bearer ")) {
+    headers.Authorization = key;
+    payload = body;
+  } else if (key?.startsWith("MEDAL-")) {
+    url = MEDAL_URL;
+    headers["Content-Type"] = "text/plain";
+    headers.Authorization = key.toUpperCase();
+    payload = body.script;
+  } else if (KONSTANT_URL) {
+    url = KONSTANT_URL;
+    headers["Content-Type"] = "text/plain";
+    try {
+      payload = atob(body.script); // Decoding the script
+    } catch {
+      return reply.status(400).send({ error: "Invalid base64 encoded script" });
+    }
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  try {
+    const response = await axios.post(url, payload, { headers });
+    reply.send(response.data);
+  } catch (error) {
+    reply
+      .code(error?.response?.status ?? 500)
+      .send(error.response?.data ?? { error: "Internal Server Error" });
+  }
+});
+
+fastify.listen({ port: process.env.PORT }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`app is listening ${fastify.server.address().port}`);
+});
