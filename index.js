cf01d84a5089df27d8d9dc555075952621847d28
htt-py <113143108+htt-py@users.noreply.github.com>
2025-01-03 22:05:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
 const fastify = require("fastify")({
-  bodyLimit: 1024 * 1024 * 1024,
+  bodyLimit: 1024 * 1024 * 1024, // 1GB
 });
 const axios = require("axios");
-const { MEDAL_URL, ORACLE_URL, KONSTANT_URL, PORT } = process.env;
-const cache = require("@fastify/caching");
-const crypto = require("crypto");
-fastify.register(cache, {
-  privacy: "public",
-  expiresIn: 3600,
-});
+const { ORACLE_URL, PORT } = process.env;
 
 fastify.post("/decompile", async (request, reply) => {
   let { key } = request.headers;
   if (!key) ({ key } = request.query);
-
-  const body = request.body;
-  const headers = { "Content-Type": "application/json" };
-  let url = ORACLE_URL;
-  let payload;
-
-  const bytecode = body.script || "";
-  const options = JSON.stringify(body.decompilerOptions || {});
-  const cacheKey = crypto
-    .createHash("sha256")
-    .update(bytecode + options)
-    .digest("hex");
-
-  const cachedResponse = await fastify.cache.get(cacheKey);
-  if (cachedResponse) {
-    console.log('found in cache')
-    return reply.send(cachedResponse);
-    
-  }
-
-  if (key?.startsWith("Bearer ")) {
-    headers.Authorization = key;
-    payload = body;
-  } else if (key?.startsWith("MEDAL-")) {
-    url = MEDAL_URL;
-    headers["Content-Type"] = "text/plain";
-    headers.Authorization = key.toUpperCase();
-    payload = body.script;
-  } else if (KONSTANT_URL) {
-    url = KONSTANT_URL;
-    headers["Content-Type"] = "text/plain";
-    try {
-      payload = Buffer.from(body.script, "base64").toString("utf-8");
-    } catch {
-      return reply.status(400).send({ error: "Invalid base64 encoded script" });
-    }
-  }
+  const headers = { "Content-Type": "application/json", Authorization: key };
 
   try {
-    const response = await axios.post(url, payload, { headers });
-    // Cache the decompiled response
-    await fastify.cache.set(cacheKey, response.data); // Cache the response for future use
-    return reply.send(response.data);
+    const response = await axios.post(ORACLE_URL, request.body, { headers });
+    reply.send(response.data);
   } catch (error) {
-    return reply
-      .code(error?.response?.status || 500)
-      .send({ error: error?.response?.data || "Internal Server Error" });
+    reply
+      .code(error?.response?.status ?? 500)
+      .send(error.response?.data ?? { error: "Internal Server Error" });
   }
 });
 
